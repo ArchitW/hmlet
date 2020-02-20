8d16d15502583dd01fdcab410bb711de664cd1ca
Archit Warghane <archit2x3@hotmail.com>
2020-02-20 17:08:10 +0530

    [Testing] script to test endpoint

--- scripts/hmlet_photo_rest_api.py
+++ scripts/hmlet_photo_rest_api.py
@@ -1,15 +1,51 @@
 import requests
 import json
+import os
 
 ENDPOINT = "http://127.0.0.1:8000/api/photo/"
+image_path = os.path.join(os.getcwd(),"300.png")
 
 
-def do(method = 'get', data={}, is_json=True):
+def do_img(method = 'get', data={}, is_json=True, img_path=None):
+
+    headers = {}
     if is_json:
-        data = json.dumps(data)
-    r = requests.request(method, ENDPOINT +"?id="+str(id), data=data)
+        headers['content-type'] = 'application/json'
+    if img_path is not None:
+        with open(image_path, 'rb') as image:
+            file_data = {
+                'image': image
+            }
+            r = requests.request(method, ENDPOINT, data=data, files=file_data, headers=headers)
+    else:
+        r = requests.request(method, ENDPOINT, data=data, headers=headers)
     print(r.text)
+    print(r.status_code)
     return r
 
 
-do(data={'id': 10})
+#do_img(method="post", data={'user': 1, 'content': ''}, is_json=False, img_path=image_path)
+do_img(method="put", data={'id':10,'user': 1, 'content': ''}, is_json=False, img_path=image_path)
+
+'''
+def do(method = 'get', data={}, is_json=True):
+    headers = {}
+    if is_json:
+        headers['content-type'] = 'application/json'
+        data = json.dumps(data)
+    r = requests.request(method, ENDPOINT, data=data, headers=headers)
+    print(r.text)
+    print(r.status_code)
+    return r
+
+
+#get
+do(data={'id': 3})
+#post
+do(method='post', data={"content": "test content", "user": 1})
+#put/patch
+do(method='put', data={'id': 6, "content": "test content", "user": 1})
+#delete
+do(method='delete', data={'id': 3})
+#do(data={'id': 300000})
+'''
--- src/photo/api/views.py
+++ src/photo/api/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.views import APIView
 from rest_framework import generics, mixins
 from rest_framework.response import Response
@@ -8,14 +10,14 @@ from ..models import Photo
 from .serializers import PhotoSerializer
 
 
-class PhotoListSearchAPIView(APIView):
-    permission_classes = []
-    authentication_classes = []
-
-    def get(self, request, format=None):
-        qs = Photo.objects.all()
-        serializer = PhotoSerializer(qs, many=True)
-        return Response(serializer.data)
+def is_json(json_data):
+    '''verifies given data is json or not'''
+    try:
+        is_json_data = json.loads(json_data)
+        is_valid = True
+    except ValueError:
+        is_valid = False
+    return is_valid
 
 
 class PhotoAPIView(
@@ -29,6 +31,7 @@ class PhotoAPIView(
     authentication_classes = []
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    passed_id =  None
 
     def get_queryset(self):
         qs = Photo.objects.all()
@@ -39,7 +42,7 @@ class PhotoAPIView(
 
     def get_object(self):
         request = self.request
-        passed_id = request.GET.get('id', None)
+        passed_id = request.GET.get('id', None) or self.passed_id
         queryset = self.get_queryset()
         obj = None
         if passed_id is not None:
@@ -47,8 +50,21 @@ class PhotoAPIView(
             self.check_object_permissions(request, obj)
         return obj
 
+    def perform_destroy(self,instance):
+        if instance is not None:
+            return instance.delete()
+        return None
+
     def get(self, request, *args, **kwargs):
-        passed_id = request.GET.get('id', None)
+        #print(request.body)
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        if is_json(request.body):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
         if passed_id is not None:
             return self.retrieve(request, *args, **kwargs)
         return super().get(request, *args, **kwargs)
