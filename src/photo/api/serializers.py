c56ea70c79a12fa85983e104bd8e828932b8bfbc
Archit Warghane <archit2x3@hotmail.com>
2020-02-20 11:10:34 +0530

    serializers

--- src/photo/api/serializers.py
+++ src/photo/api/serializers.py
@@ -1,2 +1,24 @@
 from rest_framework import serializers
-from photos.models
+from photo.models import Photo
+
+
+class PhotoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = [
+            'user',
+            'content',
+            'image'
+        ]
+
+    def validate_content(self, value):
+        pass
+
+    def validate(self, data):
+        content = data.get("content", None)
+        if content == "":
+            content = None
+        image = data.get("image", None)
+        if content is None and image is None:
+            raise serializers.ValidationError("Content or Image is required.")
+        return data
