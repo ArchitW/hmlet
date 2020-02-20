5cd7bfbd85d26b137dfdc580bcccbbd0765cc763
Archit Warghane <archit2x3@hotmail.com>
2020-02-20 10:38:48 +0530

    base model

--- src/photo/models.py
+++ src/photo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+def upload_image(instance, filename):
+    return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+
+
+class Photo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image, null=True, blank=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
