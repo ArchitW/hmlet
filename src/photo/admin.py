1a827a7dedc51ef6aec527b7fb52d39de43137c7
Archit Warghane <archit2x3@hotmail.com>
2020-02-20 16:05:57 +0530

    [feature] Photo Fields for Admin Panel

--- src/photo/admin.py
+++ src/photo/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Photo
 
-admin.site.register(Photo)
+
+class PhotoDetails(admin.ModelAdmin):
+    list_display = ("user", "image", "content", "timestamp")
+    list_filter = ("user", )
+
+
+admin.site.register(Photo, PhotoDetails)
