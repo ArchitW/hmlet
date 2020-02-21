866a0d3003e0f2f1d0f97fc347dcb944e35b9817
Archit Warghane <archit2x3@hotmail.com>
2020-02-21 19:13:09 +0530

    [fix] JWT expiration time 10 days

--- src/photo_api/restconf/main.py
+++ src/photo_api/restconf/main.py
@@ -5,9 +5,7 @@ from django.conf import settings
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
@@ -34,6 +32,8 @@ JWT_AUTH = {
 
     'JWT_PAYLOAD_GET_USER_ID_HANDLER':
     'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=10),
 
     'JWT_RESPONSE_PAYLOAD_HANDLER':
     #'rest_framework_jwt.utils.jwt_response_payload_handler',
