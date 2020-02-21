95f2103bf93771a49a9d06639445bb74df11463a
Archit Warghane <archit2x3@hotmail.com>
2020-02-21 14:18:57 +0530

    [feature] user registration

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,3 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+        ]
+
+    def create(self, validated_data):
+        user_obj = User(
+            username=validated_data.get('username'),
+            email=validated_data.get('email'))
+        user_obj.set_password(validated_data.get('password'))
+        user_obj.save()
+        return user_obj
+
+
+
+
+'''
+Should do it later, lol
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
@@ -16,8 +46,8 @@ User = get_user_model()
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(style={'input_type':'password'}, write_only=True)
-    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     token = serializers.SerializerMethodField(read_only=True)
     expires = serializers.SerializerMethodField(read_only=True)
     message = serializers.SerializerMethodField(read_only=True)
@@ -28,15 +58,32 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             'username',
             'email',
             'password',
-            'password2',
+            'password2'
             'token',
             'expires',
 
             'message'
         ]
+        extra_kwargs = {'password':{'write_only':True}}
+
+        def validate(self, data):
+            pw = data.get('password')
+            pw2 = data.pop('password2')
+            if pw != pw2:
+                raise serializers.ValidationError("password must match")
+            return data
+
+        def create(self, validated_data):
+            user_obj = User(
+                username=validated_data.get('username'),
+                email=validated_data.get('email'))
+            user_obj.set_password(validated_data.get('password'))
+            user_obj.save()
+            return user_obj
+
 
         def get_message(self, obj):
-            return "registration successful3"
+            return "registration successful"
 
         def validate_email(self, value):
             qs = User.objects.filter(email__iexact=value)
@@ -61,15 +108,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
         def validate(self, data):
             pw = data.get('password')
-            pw2 = data.pop('password2')
+            pw2 = data.get('password2')
             if pw != pw2:
                 raise serializers.ValidationError("password must match")
             return data
-
-        def create(self, validated_data):
-            user_obj = User(
-                username= validated_data.get('username'),
-                email=validated_data.get('username'))
-            user_obj.set_password(validated_data.get('password'))
-            user_obj.save()
-            return user_obj
+'''
